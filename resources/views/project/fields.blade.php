622bc8b97a0e9c8b3c5aebcf0331e3a43f6a902d
Nur Cahyo <nurcahyoputro0@gmail.com>
2019-05-29 00:01:02 +0700

    fixing bug create project

--- resources/views/project/fields.blade.php
+++ resources/views/project/fields.blade.php
@@ -59,7 +59,7 @@
                     <div class="input-group-prepend">
                         <span class="input-group-text"><i class="fas fa-database"></i></span>
                     </div>
-                    {!! Form::text('db_connection', !empty($item->db_connection) ? $item->db_connection : 'mysql', ['class' => 'form-control form-control-alternative', 'placeholder' => 'Write somethings...']) !!}
+                    {!! Form::text('db_connection', !empty($item) ? $item->db_connection : 'mysql', ['class' => 'form-control form-control-alternative', 'placeholder' => 'Write somethings...']) !!}
                 </div>
             </div>
         </div>
@@ -71,7 +71,7 @@
                     <div class="input-group-prepend">
                         <span class="input-group-text"><i class="fas fa-database"></i></span>
                     </div>
-                    {!! Form::text('db_host', !empty($item->db_host) ? $item->db_host : '127.0.0.1', ['class' => 'form-control form-control-alternative', 'placeholder' => 'Write somethings...']) !!}
+                    {!! Form::text('db_host', !empty($item) ? $item->db_host : '127.0.0.1', ['class' => 'form-control form-control-alternative', 'placeholder' => 'Write somethings...']) !!}
                 </div>
             </div>
         </div>
@@ -83,7 +83,7 @@
                     <div class="input-group-prepend">
                         <span class="input-group-text"><i class="fas fa-database"></i></span>
                     </div>
-                    {!! Form::text('db_port', !empty($item->db_port) ? $item->db_port : '3306', ['class' => 'form-control form-control-alternative', 'placeholder' => 'Write somethings...']) !!}
+                    {!! Form::text('db_port', !empty($item) ? $item->db_port : '3306', ['class' => 'form-control form-control-alternative', 'placeholder' => 'Write somethings...']) !!}
                 </div>
             </div>
         </div>
@@ -151,7 +151,7 @@
                     <div class="input-group-prepend">
                         <span class="input-group-text"><i class="fas fa-envelope"></i></span>
                     </div>
-                    {!! Form::text('mail_driver', !empty($item->mail_encryption) ? $item->mail_encryption : 'smtp', ['class' => 'form-control form-control-alternative', 'placeholder' => 'Write somethings...']) !!}
+                    {!! Form::text('mail_driver', !empty($item) ? $item->mail_encryption : 'smtp', ['class' => 'form-control form-control-alternative', 'placeholder' => 'Write somethings...']) !!}
                 </div>
             </div>
         </div>
@@ -163,7 +163,7 @@
                     <div class="input-group-prepend">
                         <span class="input-group-text"><i class="fas fa-envelope"></i></span>
                     </div>
-                    {!! Form::text('mail_host', !empty($item->mail_host) ? $item->mail_host: 'smtp.google.com', ['class' => 'form-control form-control-alternative', 'placeholder' => 'Write somethings...']) !!}
+                    {!! Form::text('mail_host', !empty($item) ? $item->mail_host: 'smtp.google.com', ['class' => 'form-control form-control-alternative', 'placeholder' => 'Write somethings...']) !!}
                 </div>
             </div>
         </div>
@@ -175,7 +175,7 @@
                     <div class="input-group-prepend">
                         <span class="input-group-text"><i class="fas fa-envelope"></i></span>
                     </div>
-                    {!! Form::text('mail_port', !empty($item->mail_encryption) ? $item->mail_encryption : '587', ['class' => 'form-control form-control-alternative', 'placeholder' => 'Write somethings...']) !!}
+                    {!! Form::text('mail_port', !empty($item) ? $item->mail_encryption : '587', ['class' => 'form-control form-control-alternative', 'placeholder' => 'Write somethings...']) !!}
                 </div>
             </div>
         </div>
@@ -211,7 +211,7 @@
                     <div class="input-group-prepend">
                         <span class="input-group-text"><i class="fas fa-envelope"></i></span>
                     </div>
-                    {!! Form::text('mail_encryption', !empty($item->mail_encryption) ? $item->mail_encryption : 'tls', ['class' => 'form-control form-control-alternative', 'placeholder' => 'Write somethings...']) !!}
+                    {!! Form::text('mail_encryption', !empty($item) ? $item->mail_encryption : 'tls', ['class' => 'form-control form-control-alternative', 'placeholder' => 'Write somethings...']) !!}
                 </div>
             </div>
         </div>
@@ -244,7 +244,7 @@
                     <div class="input-group-prepend">
                         <span class="input-group-text"><i class="fas fa-file"></i></span>
                     </div>
-                    {!! Form::number('item_per_page', !empty($item->item_per_page) ? $item->item_per_page : 15, ['class' => 'form-control form-control-alternative', 'placeholder' => 'Write somethings...']) !!}
+                    {!! Form::number('item_per_page', !empty($item) ? $item->item_per_page : 15, ['class' => 'form-control form-control-alternative', 'placeholder' => 'Write somethings...']) !!}
                 </div>
             </div>
         </div>
@@ -259,89 +259,91 @@
     </button>
 </div>
 
+@if(!empty($item))
+    <hr>
 
-<hr>
+    <h6 class="heading-small text-muted mb-4">Global Variables</h6>
 
-<h6 class="heading-small text-muted mb-4">Global Variables</h6>
+    <div class="pl-lg-4">
 
-<div class="pl-lg-4">
+        <div class="row">
 
-    <div class="row">
+            <div class="col-lg-12">
 
-        <div class="col-lg-12">
+                @foreach (App\GenerateOption::all() as $option)
 
-            @foreach (App\GenerateOption::all() as $option)
+                    <h3>
+                        <i class="fas fa-{{ $option->icon }}"></i>
+                        {{ $option->display_name }}
+                    </h3>
 
-                <h3>
-                    <i class="fas fa-{{ $option->icon }}"></i>
-                    {{ $option->display_name }}
-                </h3>
+                    <div class="table-responsive">
 
-                <div class="table-responsive">
-
-                    @if(count(DB::table('global_variables')->where('generate_option_id', $option->id)->get()) < 1)
-                        <h5 class="text-center">
-                            Global Variable not found
-                        </h5>
-                    @else
-                        <table class="table align-items-center" data-toggle="dataTable"
-                               data-form="deleteForm">
-                            <thead>
-                            <tr>
-                                <th scope="col">Name</th>
-                                <th scope="col">Value</th>
-                                <th scope="col"></th>
-                            </tr>
-                            </thead>
-                            <tbody id="project_variables{{ $item->id }}">
-                            @foreach(DB::table('global_variables')->where('generate_option_id', $option->id)->get() as $global_variable)
-
-                                @php
-                                    $variable = $item->variables()->where('variable_id', $global_variable->id)->first()
-                                @endphp
-
-                                <tr id="global_variable{!! $global_variable->id !!}">
-                                    <td>
-                                        <h5>
-                                            {{ $global_variable->name }}
-                                        </h5>
-{{--                                        {!! Form::text('name', $global_variable->name, ['class' => 'form-control form-control-alternative', 'placeholder' => 'Write somethings...']) !!}--}}
-                                    </td>
-                                    <td class="w-100">
-                                        {!! Form::text('value', !empty($variable->pivot->value) ? $variable->pivot->value : $global_variable->value, ['id' => "variable{$global_variable->id}ValueInput", 'class' => 'form-control form-control-alternative', 'placeholder' => 'Write somethings...']) !!}
-                                    </td>
-
-                                    <td>
-                                        <button
-                                            type="button"
-                                            class="btn btn-icon btn-primary btn-sm"
-                                            onclick="onFillVariable({!! $item->id !!}, {!! $global_variable->id !!})">
-                                            <span class="btn-inner--icon"><i class="fas fa-save"></i></span>
-                                            <span class="btn-inner--text">Save</span>
-                                        </button>
-                                    </td>
-
+                        @if(count(DB::table('global_variables')->where('generate_option_id', $option->id)->get()) < 1)
+                            <h5 class="text-center">
+                                Global Variable not found
+                            </h5>
+                        @else
+                            <table class="table align-items-center" data-toggle="dataTable"
+                                   data-form="deleteForm">
+                                <thead>
+                                <tr>
+                                    <th scope="col">Name</th>
+                                    <th scope="col">Value</th>
+                                    <th scope="col"></th>
                                 </tr>
-                                {{--@include('generate-options.partials.global-variable-item')--}}
-                            @endforeach
+                                </thead>
+                                <tbody id="project_variables{{ $item->id }}">
+                                @foreach(DB::table('global_variables')->where('generate_option_id', $option->id)->get() as $global_variable)
 
-                            </tbody>
-                        </table>
-                    @endif
-                </div>
+                                    @php
+                                        $variable = $item->variables()->where('variable_id', $global_variable->id)->first()
+                                    @endphp
 
-                <hr>
+                                    <tr id="global_variable{!! $global_variable->id !!}">
+                                        <td>
+                                            <h5>
+                                                {{ $global_variable->name }}
+                                            </h5>
+                                            {{--                                        {!! Form::text('name', $global_variable->name, ['class' => 'form-control form-control-alternative', 'placeholder' => 'Write somethings...']) !!}--}}
+                                        </td>
+                                        <td class="w-100">
+                                            {!! Form::text('value', !empty($variable->pivot->value) ? $variable->pivot->value : $global_variable->value, ['id' => "variable{$global_variable->id}ValueInput", 'class' => 'form-control form-control-alternative', 'placeholder' => 'Write somethings...']) !!}
+                                        </td>
 
-            @endforeach
+                                        <td>
+                                            <button
+                                                type="button"
+                                                class="btn btn-icon btn-primary btn-sm"
+                                                onclick="onFillVariable({!! $item->id !!}, {!! $global_variable->id !!})">
+                                                <span class="btn-inner--icon"><i class="fas fa-save"></i></span>
+                                                <span class="btn-inner--text">Save</span>
+                                            </button>
+                                        </td>
+
+                                    </tr>
+                                    {{--@include('generate-options.partials.global-variable-item')--}}
+                                @endforeach
+
+                                </tbody>
+                            </table>
+                        @endif
+                    </div>
+
+                    <hr>
+
+                @endforeach
+
+            </div>
 
         </div>
 
+        <button class="btn btn-icon btn-3 btn-primary" type="submit">
+            <span class="btn-inner--icon"><i class="ni ni-send"></i></span>
+
+            <span class="btn-inner--text">Send</span>
+
+        </button>
     </div>
 
-    <button class="btn btn-icon btn-3 btn-primary" type="submit">
-        <span class="btn-inner--icon"><i class="ni ni-send"></i></span>
-
-        <span class="btn-inner--text">Send</span>
-
-    </button>
-</div>
+@endif
